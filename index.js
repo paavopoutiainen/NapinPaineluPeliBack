4569829499ed6cfbee0d8d1dc71f994fe4ef660f
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-02-27 21:17:41 +0200

    added counter and function for giving points as a response to the click event from client, emitting back the increased points

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const server = app.listen(3001, () => {
     console.log("listening 3001")
 })
 
+let counter = 0
+
+const givePoints = (counter) => {
+    if(counter % 500 === 0){
+        return 250
+    } else if (counter % 100 === 0){
+        return 40
+    } else if(counter % 10 === 0){
+        return 5
+    } else return 0
+}
+
 const io = socket(server)
 
 io.on("connection", (socket) => {
+  
+    socket.on("click", () => {
+        counter++;
+        points = givePoints(counter)
+        console.log(counter)
+        socket.emit("clickResponse", { pointsIncreased: points})
+    })
 })
 
